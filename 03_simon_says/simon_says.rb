c283ccdfc3933503b6bb32d624d93d99b00429f1
Neale Brown <nbrown3@sky.com>
2017-06-18 12:18:25 +0100

    final commit - used titleize from stackoverflow

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,37 @@
-#write your code here
+def echo(word)
+return word
+
+end
+
+def shout(shout_word)
+return shout_word.upcase
+end
+
+def repeat(repeat_word, num = 2)
+    repeat_word = repeat_word + " "
+    repeat_word2 = repeat_word * num
+return repeat_word2.strip
+end
+
+def start_of_word (word, num)
+    start_word = word[0,num]
+    return start_word
+end
+
+def first_word (words)
+    words_first = words.split()
+    return words_first[0]
+end
+
+def titleize(str)
+    str.capitalize!  # capitalize the first word in case it is part of the no words array
+    words_no_cap = ["and", "or", "the", "over", "to", "the", "a", "but"]
+    phrase = str.split(" ").map {|word|
+        if words_no_cap.include?(word)
+            word
+        else
+            word.capitalize
+        end
+    }.join(" ") # I replaced the "end" in "end.join(" ") with "}" because it wasn't working in Ruby 2.1.1
+  phrase  # returns the phrase with all the excluded words
+end
