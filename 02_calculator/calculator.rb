89266e9b387545f35f9bc751d99d62806b7148c7
Neale Brown <nbrown3@sky.com>
2017-06-18 11:33:26 +0100

    final commit all errors corrected including factorial exercise.

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,44 @@
 #write your code here
+def add (num1, num2)
+    return num1+num2
+end
+
+def subtract (num1, num2)
+    return num1 - num2
+end
+
+def sum(array1)
+    total=0
+    array1.each do |i|
+        total = total + i
+    end
+    return total
+end
+
+def multiply(num1,num2)
+    return num1 * num2
+end
+
+def multiplyArray (array1)
+    total = 1
+    array1.each do |i|
+        total = total * i
+    end
+    return total
+end
+
+def power(num1,num2)
+    return num1 ** num2
+end
+
+def factorial(num)
+    total = 1
+    if num == 0
+        total = 0
+    elsif num != 0
+        1.upto(num) do |i|
+        total = total * i
+    end
+    return total
+    end
+end
