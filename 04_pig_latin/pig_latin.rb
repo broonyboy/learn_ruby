9344286af70c82f12bbf6fe510a93bfe33f98945
Neale Brown <nbrown3@sky.com>
2017-06-19 21:03:58 +0100

    final commit

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,33 @@
 #write your code here
+def translate sentence
+
+vowels = 'a e i o u y'
+translation = []
+words = sentence.split
+words.each do |word|
+    if vowels.include? word[0]
+        translation.push(word + 'ay')
+    elsif 'qu QU Qu'.include? word[0..1] and vowels.include? word[2]
+        translation.push(word[2..-1] + word[0..1] + 'ay')
+    elsif vowels.include? word[1]
+        translation.push(word[1..-1] + word[0] + 'ay')
+    elsif 'qu QU Qu'.include? word[1..2]
+        translation.push(word[3..-1] + word[0..2] + 'ay')
+    elsif vowels.include? word[2]
+        translation.push(word[2..-1] + word[0..1] + 'ay')
+    else
+        translation.push(word[3..-1] + word[0..2] + 'ay')
+    end
+end
+translation.each do |word|
+    if word != word.downcase
+        word.downcase!
+        word.capitalize!
+    end
+    if word.index(/[,;:.?!]/) != nil
+        punctuation_index = word.index(/[,;:.?!]/)
+
+    end
+end
+translation.join(' ')
+end
